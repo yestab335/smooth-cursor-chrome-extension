e401a92a9a99e0c96a32db508578597215a4ec83
yestab335 <yestab335@gmail.com>
2022-08-26 22:46:30 -0400

    Smooth Cursor Manipuation For Google Docs

---
+++ scripts/google-docs.js
@@ -0,0 +1,3 @@
+var cursor = document.getElementsByClassName("kix-cursor")[0];
+var cursor_style = cursor.getAttribute("style");
+cursor.setAttribute('style', cursor_style+" transition: all 310ms;");
